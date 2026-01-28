eb48df47aa49f1bd492b59f9b03bc7b745754274
SAI PRAVARSHAK <aanakapa@student.gitam.edu>
2026-01-28 11:35:59 +0530

    conditional statement.py

--- conditional statement.py
+++ conditional statement.py
@@ -4,3 +4,9 @@ if age >= 18:
     print("You are eligible to vote.")
 else:
     print(f"You must wait {18 - age} more years to vote.")
+
+#output
+Please enter your age: 17
+You must wait 1 more years to vote.
+
+=== Code Execution Successful ===
